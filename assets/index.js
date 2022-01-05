68e6dd9b06e07e2020212787f5ec28e83c33b1f3
Marco Rivera <marcogeofis@gmail.com>
2022-01-05 02:52:27 -0600

    Analizamos que es Scope, Scope-global, Scope-block, function-Scope y añadí un module-Scope donde usamos las propiedades de muted para reproducir un video sin sonido

--- assets/index.js
+++ assets/index.js
@@ -1,26 +1,9 @@
-function MediaPlayer(config) {
-    this.media = config.el;
-  }
-  
-  MediaPlayer.prototype.play = function() {
-    this.media.play();
-  };
-  
-  MediaPlayer.prototype.pause = function() {
-    this.media.pause();
-  };
-  
-  MediaPlayer.prototype.togglePlay = function() {
-    if (this.media.paused) {
-      this.play();
-    } else {
-      this.pause();
-    }
-  };
-  
-  const video = document.querySelector('video');
-  const player = new MediaPlayer({ el: video });
-  
-  const button = document.querySelector('button');
-  button.onclick = () => player.togglePlay();
+import MediaPlayer from "./MediaPlayers.js";
+import AutoPlay from "./plugins/AutoPlay.js";
+
+const video = document.querySelector('video');
+const player = new MediaPlayer({ el: video, plugins: [ new AutoPlay()] });
+
+const button = document.querySelector('button');
+button.onclick = () => player.togglePlay();
   
